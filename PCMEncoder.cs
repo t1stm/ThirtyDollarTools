2c48b2db65980a5289516e558772b7aa7eb5d145
Kristian Gergov <kristian.gergov1@gmail.com>
2022-07-18 21:02:05 +0300

    Formatted the code.

--- PCMEncoder.cs
+++ PCMEncoder.cs
@@ -67,14 +67,17 @@ namespace ThirtyDollarWebsiteConverter
                                     volume = ev.Value;
                                     break;
                             }
+
                             break;
                     }
+
                     ev.Volume = volume;
                 }
+
                 Composition.Events.RemoveAll(e => e.SoundEvent is SoundEvent.Volume);
             }
         }
-        
+
         public void Start()
         {
             if (Composition == null) throw new Exception("Null Composition");
@@ -82,7 +85,7 @@ namespace ThirtyDollarWebsiteConverter
             var position = (ulong) (SampleRate / (bpm / 60));
             var count = Composition.Events.Count;
             CalculateVolume();
-            
+
             for (var i = 0; i < Composition!.Events.Count; i++)
             {
                 var ev = Composition.Events[i];
@@ -103,6 +106,7 @@ namespace ThirtyDollarWebsiteConverter
                                     bpm = ev.Value;
                                     break;
                             }
+
                             count--;
                             Console.WriteLine($"BPM is now: {bpm}");
                             continue;
@@ -117,6 +121,7 @@ namespace ThirtyDollarWebsiteConverter
                                 {
                                     continue;
                                 }
+
                                 i = j - 1;
                                 break; // Ooga booga, I am retarded. I've been debugging this for loop for two hours now. How could've I forgotten to add the break?
                             }
@@ -135,6 +140,7 @@ namespace ThirtyDollarWebsiteConverter
                                 Console.WriteLine($"Unable to target with id: {ev.Value}");
                                 continue;
                             }
+
                             i = Composition.Events.IndexOf(item) - 1;
                             Console.WriteLine($"Jumping to element: ({i}) - {Composition.Events[i]}");
                             count--;
@@ -152,38 +158,50 @@ namespace ThirtyDollarWebsiteConverter
                             ev.Loop = ev.OriginalLoop;
                             count--;
                             continue;
-                        
+
                         case SoundEvent.CutAllSounds:
-                            for (var j = position + (ulong) (SampleRate / (bpm / 60)); j < (ulong) PcmBytes.LongLength; j++)
+                            for (var j = position + (ulong) (SampleRate / (bpm / 60));
+                                j < (ulong) PcmBytes.LongLength;
+                                j++)
                             {
                                 PcmBytes[j] = 0;
                             }
+
                             count--;
                             continue;
-                        
+
                         case SoundEvent.None or SoundEvent.LoopTarget or SoundEvent.SetTarget or SoundEvent.Volume:
                             count--;
                             continue;
-                        
+
                         case SoundEvent.Combine:
                             position -= (ulong) (SampleRate / (bpm / 60));
                             continue;
-                        
-                        default: 
+
+                        default:
                             position += (ulong) (SampleRate / (bpm / 60));
                             break;
                     }
-                    
+
                     var index = position - position % 2;
                     Console.WriteLine($"Processing Event: [{index}] - \"{ev}\"");
                     HandleProcessing(ev, index, -1);
-                    if (ev.Loop > 1)
+                    switch (ev.SoundEvent)
                     {
-                        ev.Loop--;
-                        i--;
-                        continue;
+                        case not (SoundEvent.Speed or SoundEvent.GoToLoop or SoundEvent.JumpToTarget or SoundEvent.Pause
+                            or SoundEvent.CutAllSounds or SoundEvent.None or SoundEvent.LoopTarget or
+                            SoundEvent.SetTarget or SoundEvent.Volume or SoundEvent.Combine):
+                            
+                            if (ev.Loop > 1)
+                            {
+                                ev.Loop--;
+                                i--;
+                                continue;
+                            }
+
+                            ev.Loop = ev.OriginalLoop;
+                            continue;
                     }
-                    ev.Loop = ev.OriginalLoop;
                 }
                 catch (Exception e)
                 {
@@ -205,21 +223,22 @@ namespace ThirtyDollarWebsiteConverter
         {
             try
             {
-                ProcessedSample sample = ev.Value == 0 ? new ProcessedSample
-                {
-                    SampleData = Program.Samples[ev.SampleId],
-                    Volume = ev.Volume
-                } : new ProcessedSample
-                {
-                    SampleData = Resample(Program.Samples[ev.SampleId], SampleRate, (uint) (SampleRate / Math.Pow(2, ev.Value / 12)), Channels),
-                    Volume = ev.Volume
-                };
+                ProcessedSample sample = ev.Value == 0
+                    ? new ProcessedSample {SampleData = Program.Samples[ev.SampleId], Volume = ev.Volume}
+                    : new ProcessedSample
+                    {
+                        SampleData = Resample(Program.Samples[ev.SampleId], SampleRate,
+                            (uint) (SampleRate / Math.Pow(2, ev.Value / 12)), Channels),
+                        Volume = ev.Volume
+                    };
 
                 var size = breakAtIndex == -1 ? sample.SampleLength : (ulong) breakAtIndex;
                 for (sample.ProcessedChunks = 0; sample.ProcessedChunks < size; sample.ProcessedChunks++)
                 {
                     if (breakAtIndex != -1 && sample.ProcessedChunks >= (ulong) breakAtIndex) return;
-                    AddOrChangeByte((float) ((sample.SampleData?[sample.ProcessedChunks] ?? 0) * (sample.Volume * 0.5 / 100)) / 32768f, index + sample.ProcessedChunks);
+                    AddOrChangeByte(
+                        (float) ((sample.SampleData?[sample.ProcessedChunks] ?? 0) * (sample.Volume * 0.5 / 100)) /
+                        32768f, index + sample.ProcessedChunks);
                 }
             }
             catch (Exception e)
@@ -245,42 +264,41 @@ namespace ThirtyDollarWebsiteConverter
         }
 
         // Original Source: https://github.com/cpuimage/resampler
-        
-        private unsafe ulong Resample32BitFloat(float *input, float* output, uint inSampleRate, uint outSampleRate, ulong inputSize, uint channels) {
-            
-            if (input == null)
-                return 0;
+
+        private unsafe ulong Resample32BitFloat(float* input, float* output, uint inSampleRate, uint outSampleRate,
+            ulong inputSize, uint channels)
+        {
+            if (input == null) return 0;
             var outputSize = (ulong) (inputSize * (double) outSampleRate / inSampleRate);
             outputSize -= outputSize % channels;
-            if (output == null)
-                return outputSize;
+            if (output == null) return outputSize;
             var stepDist = inSampleRate / (double) outSampleRate;
             const ulong fixedFraction = (ulong) 1 << 32;
             const double normFixed = 1.0 / ((ulong) 1 << 32);
             var step = (ulong) (stepDist * fixedFraction + 0.5);
             ulong curOffset = 0;
-            for (uint i = 0; i < outputSize; i += 1) {
-                for (uint c = 0; c < channels; c += 1) {
-                    *output++ = (float) (input[c] + (input[c + channels] - input[c]) * (
-                                (curOffset >> 32) + (curOffset & (fixedFraction - 1)) * normFixed
-                            )
-                        );
+            for (uint i = 0; i < outputSize; i += 1)
+            {
+                for (uint c = 0; c < channels; c += 1)
+                {
+                    *output++ = (float) (input[c] + (input[c + channels] - input[c]) *
+                        ((curOffset >> 32) + (curOffset & (fixedFraction - 1)) * normFixed));
                 }
+
                 curOffset += step;
                 input += (curOffset >> 32) * channels;
                 curOffset &= fixedFraction - 1;
             }
+
             return outputSize;
-            
         }
-        
+
         private unsafe ulong Resample16Bit(short* input, short* output, uint inSampleRate, uint outSampleRate,
             ulong inputSize, uint channels)
         {
             var outputSize = (ulong) (inputSize * (double) outSampleRate / inSampleRate);
             outputSize -= outputSize % channels;
-            if (output == null)
-                return outputSize;
+            if (output == null) return outputSize;
             var stepDist = (double) inSampleRate / outSampleRate;
             const ulong fixedFraction = (ulong) 1 << 32;
             const double normFixed = 1.0 / ((ulong) 1 << 32);
@@ -309,7 +327,7 @@ namespace ThirtyDollarWebsiteConverter
             foreach (var data in PcmBytes) stream.Write((short) (data * 32768));
             stream.Close();
         }
-        
+
         public MemoryStream WriteAsWavStream()
         {
             var ms = new MemoryStream();
@@ -325,12 +343,12 @@ namespace ThirtyDollarWebsiteConverter
 
         private void AddWavHeader(BinaryWriter writer)
         {
-            writer.Write(new[]{'R','I','F','F'}); // RIFF Chunk Descriptor
+            writer.Write(new[] {'R', 'I', 'F', 'F'}); // RIFF Chunk Descriptor
             writer.Write(4 + 8 + 16 + 8 + PcmBytes.Length * 2); // Sub Chunk 1 Size
             //Chunk Size 4 bytes.
-            writer.Write(new[]{'W','A','V','E'});
+            writer.Write(new[] {'W', 'A', 'V', 'E'});
             // fmt sub-chunk
-            writer.Write(new[]{'f','m','t',' '});
+            writer.Write(new[] {'f', 'm', 't', ' '});
             writer.Write(16); // Sub Chunk 1 Size
             writer.Write((short) 1); // Audio Format 1 = PCM
             writer.Write((short) Channels); // Audio Channels
@@ -339,9 +357,8 @@ namespace ThirtyDollarWebsiteConverter
             writer.Write((short) (Channels * 2)); // Block Align
             writer.Write((short) 16); // Bits per Sample
             // data sub-chunk
-            writer.Write(new[]{'d','a','t','a'});
+            writer.Write(new[] {'d', 'a', 't', 'a'});
             writer.Write(PcmBytes.Length * Channels * 2); // Sub Chunk 2 Size.
-            
         }
     }
 }
