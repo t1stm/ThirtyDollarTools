242e74f358abb89421181dfdb3900361e52576fe
Kristian Gergov <kristian.gergov1@gmail.com>
2022-07-24 22:19:08 +0300

    Laid the ground works for the new DPCM Encoder.

---
+++ ThirtyDollarEncoder/DPCM/DPCMEncoder.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtyDollarEncoder.DPCM
+{
+    // This class is a meme / shitpost.
+    public class DPCMEncoder
+    {
+        public byte[]? Encode(IEnumerable<float> data)
+        {
+            return Encode(data.Select(b => (short) (b * 32768)));
+        }
+        public byte[]? Encode(IEnumerable<short> data)
+        {
+            var audio = data.ToArray();
+            short oldSample = 0;
+            ushort placement = 0;
+            var output = new byte[(int) (audio.LongLength / 8) + 1];
+            for (long i = 0; i < audio.LongLength; i++)
+            {
+                var j = (int) Math.Floor((decimal) (i * 0.125));
+                output[j] |= (byte) (output[j] | (oldSample > audio[i] ? 0 : 1) << placement);
+                if (++placement > 8) placement = 0;
+                oldSample = audio[i];
+            }
+            // For now this won't do anything so don't expect too much. I am currently experimenting.
+            return null;
+        }
+
+        public short[]? DecodeToPcm(IEnumerable<byte> data, PCMSettings settings)
+        {
+            var audio = data.ToArray(); // I hope that it's audio.
+            
+            return null;
+        }
+    }
+}
---
+++ ThirtyDollarEncoder/PCMSettings.cs
@@ -0,0 +1,9 @@
+namespace ThirtyDollarEncoder
+{
+    public struct PCMSettings
+    {
+        public int SampleRate { get; set; }
+        public int BitsPerSample { get; set; }
+        public int Channels { get; set; }
+    }
+}
