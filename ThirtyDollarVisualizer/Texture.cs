061513f3c13898215ee93056703d3306f67b15c3
Kristian Gergov <kristian.gergov1@gmail.com>
2023-03-30 00:58:55 +0300

    Started work on loading textures

--- ThirtyDollarVisualizer/Texture.cs
+++ ThirtyDollarVisualizer/Texture.cs
@@ -4,11 +4,35 @@ namespace ThirtyDollarVisualizer;
 
 public class Texture
 {
+    private Color[][] Pixels;
     public Texture(string path)
     {
         if (!File.Exists(path)) throw new FileNotFoundException(
             $"Texture file for path \'{path}\' not found. Working directory is \'{Directory.GetCurrentDirectory()}\'.");
         var image = Image.FromFile(path);
+        var bitmap = new Bitmap(image);
         
+        var height = bitmap.Height;
+        var width = bitmap.Width;
+        var pixels = new Color[height][];
+        
+        for (var x = 0; x < height; x++)
+        {
+            pixels[x] = new Color[width];
+            for (var y = 0; y < width; y++)
+            {
+                pixels[x][y] = bitmap.GetPixel(x, y);
+            }
+        }
+        
+        bitmap.Dispose();
+        image.Dispose();
+
+        Pixels = pixels;
+    }
+
+    public void Render()
+    {
+        // TODO: Implement behavior
     }
 }
