3ce134916dce3328f7a48e7361139472bb3ec268
Kristian Gergov <kristian@gergov.bg>
2023-08-12 23:12:08 +0300

    Add different tests.
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

---
+++ ThirtyDollarEncoder.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using ThirtyDollarConverter;
+using ThirtyDollarEncoder.DPCM;
+using ThirtyDollarEncoder.PCM;
+using ThirtyDollarEncoder.Wave;
+
+namespace ThirtyDollarEncoder.Tests;
+
+public class Tests
+{
+    public const string TestingFileLocation =
+        "/home/kris/RiderProjects/ThirtyDollarWebsiteConverter/ThirtyDollarDebugApp/bin/Release/net7.0/Export/(Radiotomatosauce99) amogus of impostsussy [Ultrakill Act 2].🗿.wav";
+
+    public AudioData<short> TestingData = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        using var fs = File.OpenRead(TestingFileLocation); 
+        var decoder = new WaveDecoder();
+        var data = decoder.Read(fs);
+
+        TestingData = data.ReadAsInt16Array(false) ?? throw new NullReferenceException();
+    }
+
+    [Test]
+    public void Test_DPCM_Functionality()
+    {
+        var left_channel = DPCMEncoder.Encode(TestingData.GetChannel(0));
+        File.WriteAllBytes("./funny.dpcm", left_channel);
+        
+        var decoded = DPCMDecoder.DecodeToPcm(left_channel);
+        var audio = decoded.ReadAsFloat32Array(false) ?? throw new NullReferenceException();
+        
+        var enc = new PcmEncoder(null!, null!, new EncoderSettings
+        {
+            SampleRate = 48000,
+            Channels = 1
+        });
+        enc.WriteAsWavFile("./export.wav", audio);
+
+        Assert.Pass();
+    }
+}
