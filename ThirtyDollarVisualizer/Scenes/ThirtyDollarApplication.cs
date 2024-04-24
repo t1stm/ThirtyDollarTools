5226f949df3477ee6dfae86e627d6bfdd7235f40
Kristian Gergov <kristian@gergov.bg>
2024-04-24 16:24:52 +0300

    Bumped version to 1.1.2
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- ThirtyDollarVisualizer/Scenes/ThirtyDollarApplication.cs
+++ ThirtyDollarVisualizer/Scenes/ThirtyDollarApplication.cs
@@ -197,7 +197,7 @@ public class ThirtyDollarApplication : ThirtyDollarWorkflow, IScene
                     """
         }.WithPosition((10, 0f, 0));
 
-        const string version_string = "1.1.1";
+        const string version_string = "1.1.2";
         var text = new StaticText
         {
             FontStyle = FontStyle.Bold,
