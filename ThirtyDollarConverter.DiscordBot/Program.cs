8ece4258f0c835418f0774435026e6f63f442c5c
Kristian Gergov <kristian@gergov.bg>
2025-04-25 03:18:56 +0300

    A discord bot for converting sequences and changes related to it.
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

---
+++ ThirtyDollarConverter.DiscordBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.SlashCommands;
+using ThirtyDollarConverter;
+using ThirtyDollarConverter.DiscordBot;
+
+var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+if (string.IsNullOrEmpty(token))
+{
+    Console.WriteLine("No Discord token provided.");
+    return;
+}
+
+var samples_location = Environment.GetEnvironmentVariable("SAMPLES_LOCATION");
+if (!string.IsNullOrEmpty(samples_location))
+    Static.SampleHolder = new SampleHolder
+    {
+        DownloadLocation = samples_location
+    };
+
+Console.WriteLine("Starting Discord Bot...");
+Console.WriteLine($"Using token: {token}");
+
+await Static.SampleHolder.LoadSampleList();
+await Static.SampleHolder.DownloadSamples();
+Static.SampleHolder.LoadSamplesIntoMemory();
+
+var builder = DiscordClientBuilder.CreateDefault(token, TextCommandProcessor.RequiredIntents | SlashCommandProcessor.RequiredIntents);
+// yes its deprecated. i know man
+builder.UseSlashCommands(setup =>
+{
+    setup.RegisterCommands<SlashCommands>();
+});
+
+var client = builder.Build();
+
+await client.ConnectAsync().ConfigureAwait(false);
+await Task.Delay(-1);
