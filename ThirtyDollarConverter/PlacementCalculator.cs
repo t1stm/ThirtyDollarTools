af1badda95d0f3cbc0b6b0be965572ee6806d452
Kristian Gergov <kristian@gergov.bg>
2025-05-09 02:14:05 +0300

    Make a local sequence copy when using the PlacementCalculator to avoid modifying the passed-down sequence.
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- ThirtyDollarConverter/PlacementCalculator.cs
+++ ThirtyDollarConverter/PlacementCalculator.cs
@@ -73,6 +73,8 @@ public class PlacementCalculator
     public IEnumerable<Placement> CalculateOne(Sequence sequence, ulong? start_time = null)
     {
         if (sequence == null) throw new Exception("Null Sequence");
+        sequence = sequence.Copy();
+        
         var bpm = 300.0;
         var transpose = 0.0;
         var global_volume = 100.0;
