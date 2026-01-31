6ea316483ef46b3ebefaf6b2542be73273fe4922
Kristian Gergov <kristian@gergov.bg>
2026-01-31 14:46:12 +0200

    feat: log OpenGL information during game initialization
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- ThirtyDollarVisualizer.Engine/Game.cs
+++ ThirtyDollarVisualizer.Engine/Game.cs
@@ -60,6 +60,8 @@ public class Game : GameWindow
         base.OnLoad();
         GetGLInfo(GLInfo);
 
+        Logger.Information("OpenGL Info: {@GLInfo}", GLInfo);
+
         GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
         GL.Enable(EnableCap.Multisample);
 
