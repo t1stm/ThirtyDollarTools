aa261cb256fa8c7665206d17ebfe2f58fd645886
Kristian Gergov <kristian@gergov.bg>
2026-05-06 02:30:56 +0300

    docs: improve comment clarity for StringFormatter's memory efficiency explanation
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- Sundex/Sundex.Engine/Text/Allocationless/StringFormatter.cs
+++ Sundex/Sundex.Engine/Text/Allocationless/StringFormatter.cs
@@ -3,7 +3,7 @@ namespace Sundex.Engine.Text.Allocationless;
 /// <summary>
 ///     A formatter that can be used to format strings with parameters.
 ///     100% allocation-free due to the use of span-based operations and dictionary lookups when updating the contents.
-///     Might be a bit slower than the standard string formatter, but it's a lot more memory efficient.
+///     Might be a bit slower than the standard string formatter, but it's a lot more memory efficient allocation-wise.
 /// </summary>
 public class StringFormatter
 {
