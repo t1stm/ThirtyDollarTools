035b8205384c1a7210c02795535ecacbdb876e11
Kristian Gergov <kristian@gergov.bg>
2026-06-15 09:19:46 +0300

    refactor: make the Program.cs files top-level and removed ThirtyDollarConverter.Debug
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- Converter/ThirtyDollarConverter.GUI/Program.cs
+++ Converter/ThirtyDollarConverter.GUI/Program.cs
@@ -1,26 +1,10 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using ThirtyDollarGUI;
 
-namespace ThirtyDollarGUI;
-
-internal static class Program
-{
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    public static void Main(string[] args)
-    {
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-    }
-
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-    {
-        return AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace()
-            .UseReactiveUI();
-    }
-}
+AppBuilder.Configure<App>()
+    .UsePlatformDetect()
+    .WithInterFont()
+    .LogToTrace()
+    .UseReactiveUI()
+    .StartWithClassicDesktopLifetime(args);
--- Visualizer/ThirtyDollarVisualizer/Program.cs
+++ Visualizer/ThirtyDollarVisualizer/Program.cs
@@ -1,6 +1,8 @@
 // Warm thanks to The Cherno
 // https://youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
 
+#region Usings 
+
 using System.Reflection;
 using CommandLine;
 using DrumMasterScene;
@@ -21,233 +23,174 @@ using Shared.Audio.OpenAL;
 using SixLabors.ImageSharp;
 using Sundex.Components;
 using Sundex.Engine;
+using ThirtyDollarVisualizer;
 using ThirtyDollarVisualizer.VisualizerSettings;
 using VisualizerScene;
 
-namespace ThirtyDollarVisualizer;
+#endregion
 
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        string? sequence = null;
-        bool no_audio;
-        AudioContext? audio_context = null;
-        var width = 1600;
-        var height = 840;
-        int? fps = null;
-        float? scale = null;
-        string? greeting = null;
-        int? event_size = null;
-        int? event_margin = null;
-        int? line_amount = null;
-        string? settings_location = null;
-        bool? transparent_framebuffer = null;
+string? sequence = null;
+bool no_audio;
+AudioContext? audio_context = null;
+var width = 1600;
+var height = 840;
+int? fps = null;
+float? scale = null;
+string? greeting = null;
+int? event_size = null;
+int? event_margin = null;
+int? line_amount = null;
+string? settings_location = null;
+bool? transparent_framebuffer = null;
 
 #if RELEASE
         const string logFilePath = "Visualizer_Release.log";
 #endif
 #if DEBUG
-        const string logFilePath = "Visualizer_Debug.log";
+const string logFilePath = "Visualizer_Debug.log";
 #endif
 
-        var serilogLogger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .WriteTo.Console(new ExpressionTemplate(
-                "[{@t:HH:mm:ss} {@l:u3}" +
-                "{#if SourceContext is not null} {Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}{#end}] {@m}\n{@x}",
-                theme: TemplateTheme.Code))
-            .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day,
-                rollOnFileSizeLimit: true, fileSizeLimitBytes: 100_000_000)
-            .MinimumLevel.Debug()
-            .CreateLogger();
+var serilogLogger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new ExpressionTemplate(
+        "[{@t:HH:mm:ss} {@l:u3}" +
+        "{#if SourceContext is not null} {Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)}{#end}] {@m}\n{@x}",
+        theme: TemplateTheme.Code))
+    .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day,
+        rollOnFileSizeLimit: true, fileSizeLimitBytes: 100_000_000)
+    .MinimumLevel.Debug()
+    .CreateLogger();
 
 
-        Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(options =>
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(options =>
+    {
+        sequence = options.Input;
+        no_audio = options.NoAudio;
+        width = options.Width ?? width;
+        height = options.Height ?? height;
+        fps = options.Fps;
+        scale = options.Scale;
+        greeting = options.Greeting;
+        event_size = options.EventSize;
+        event_margin = options.EventMargin;
+        line_amount = options.LineAmount;
+        settings_location = options.SettingsLocation;
+        transparent_framebuffer = options.TransparentFramebuffer;
+
+        audio_context = no_audio
+            ? new NullAudioContext()
+            : options.AudioBackend switch
             {
-                sequence = options.Input;
-                no_audio = options.NoAudio;
-                width = options.Width ?? width;
-                height = options.Height ?? height;
-                fps = options.Fps;
-                scale = options.Scale;
-                greeting = options.Greeting;
-                event_size = options.EventSize;
-                event_margin = options.EventMargin;
-                line_amount = options.LineAmount;
-                settings_location = options.SettingsLocation;
-                transparent_framebuffer = options.TransparentFramebuffer;
+                "null" => new NullAudioContext(),
+                "openal" => new OpenALContext(serilogLogger),
+                "bass" => new BassAudioContext(serilogLogger),
+                _ => null
+            };
+    });
 
-                audio_context = no_audio
-                    ? new NullAudioContext()
-                    : options.AudioBackend switch
-                    {
-                        "null" => new NullAudioContext(),
-                        "openal" => new OpenALContext(serilogLogger),
-                        "bass" => new BassAudioContext(serilogLogger),
-                        _ => null
-                    };
-            });
+Configuration.Default.PreferContiguousImageBuffers = true;
 
-        Configuration.Default.PreferContiguousImageBuffers = true;
+if (sequence != null && !File.Exists(sequence))
+{
+    serilogLogger.Warning("Unable to find specified sequence. Running without a specified sequence");
+    sequence = null;
+}
 
-        if (sequence != null && !File.Exists(sequence))
-        {
-            serilogLogger.Warning("Unable to find specified sequence. Running without a specified sequence");
-            sequence = null;
-        }
+SettingsHandler.Load(settings_location ?? "./Settings.30$");
+var settings = SettingsHandler.Settings;
 
-        SettingsHandler.Load(settings_location ?? "./Settings.30$");
-        var settings = SettingsHandler.Settings;
+if (settings.TransparentFramebuffer != transparent_framebuffer && transparent_framebuffer.HasValue)
+    settings.TransparentFramebuffer = transparent_framebuffer.Value;
 
-        if (settings.TransparentFramebuffer != transparent_framebuffer && transparent_framebuffer.HasValue)
-            settings.TransparentFramebuffer = transparent_framebuffer.Value;
+if (line_amount.HasValue) settings.LineAmount = line_amount.Value;
+if (event_size.HasValue) settings.EventSize = event_size.Value;
+if (event_margin.HasValue) settings.EventMargin = event_margin.Value;
 
-        if (line_amount.HasValue) settings.LineAmount = line_amount.Value;
-        if (event_size.HasValue) settings.EventSize = event_size.Value;
-        if (event_margin.HasValue) settings.EventMargin = event_margin.Value;
+var gameWindowSettings = new GameWindowSettings
+{
+    UpdateFrequency = fps ?? 0
+};
 
-        var gameWindowSettings = new GameWindowSettings
-        {
-            UpdateFrequency = fps ?? 0
-        };
-
-        // ReSharper disable once ConvertToConstant.Local
-        var contextFlags = ContextFlags.ForwardCompatible;
+// ReSharper disable once ConvertToConstant.Local
+var contextFlags = ContextFlags.ForwardCompatible;
 
 #if DEBUG
-        contextFlags |= ContextFlags.Debug;
+contextFlags |= ContextFlags.Debug;
 #endif
 
-        if (fps == null && !settings.UseVsync)
-            fps = 0;
+if (fps == null && !settings.UseVsync)
+    fps = 0;
 
-        var nativeWindowSettings = new NativeWindowSettings
+var nativeWindowSettings = new NativeWindowSettings
+{
+    Icon = null,
+    API = ContextAPI.OpenGL,
+    Profile = ContextProfile.Core,
+    Flags = contextFlags,
+    APIVersion = new Version(3, 3),
+    Title = "Thirty Dollar Visualizer",
+    WindowState = WindowState.Normal,
+    WindowBorder = WindowBorder.Resizable,
+    TransparentFramebuffer = settings.TransparentFramebuffer,
+    Vsync = fps == null ? VSyncMode.On : VSyncMode.Off,
+    ClientSize = (width, height)
+};
+
+Assembly[] assemblies =
+[
+    Assembly.GetExecutingAssembly(),
+    SharedAssembly.Assembly,
+    ComponentsAssembly.Assembly,
+    LoaderAssembly.Assembly,
+    HomeAssembly.Assembly,
+    VisualizerAssembly.Assembly,
+    EditorAssembly.Assembly,
+    DrumMasterAssembly.Assembly,
+    SettingsAssembly.Assembly
+];
+
+var game = new Game(serilogLogger, assemblies, gameWindowSettings, nativeWindowSettings,
+    "ThirtyDollarVisualizer");
+game.Globals.Set("visualizer-settings", settings);
+
+if (game.TryGetScreenScale(out var horizontal_scale, out var vertical_scale) &&
+    settings.AutomaticScaling) scale ??= (horizontal_scale + vertical_scale) / 2f;
+
+game.Enqueue(instance =>
+{
+    instance.SceneManager.LoadScene<Loader>("loader",
+        _ => new Loader(instance, audio_context)
         {
-            Icon = null,
-            API = ContextAPI.OpenGL,
-            Profile = ContextProfile.Core,
-            Flags = contextFlags,
-            APIVersion = new Version(3, 3),
-            Title = "Thirty Dollar Visualizer",
-            WindowState = WindowState.Normal,
-            WindowBorder = WindowBorder.Resizable,
-            TransparentFramebuffer = settings.TransparentFramebuffer,
-            Vsync = fps == null ? VSyncMode.On : VSyncMode.Off,
-            ClientSize = (width, height)
-        };
-
-        Assembly[] assemblies =
-        [
-            Assembly.GetExecutingAssembly(),
-            SharedAssembly.Assembly,
-            ComponentsAssembly.Assembly,
-            LoaderAssembly.Assembly,
-            HomeAssembly.Assembly,
-            VisualizerAssembly.Assembly,
-            EditorAssembly.Assembly,
-            DrumMasterAssembly.Assembly,
-            SettingsAssembly.Assembly
-        ];
-
-        var game = new Game(serilogLogger, assemblies, gameWindowSettings, nativeWindowSettings,
-            "ThirtyDollarVisualizer");
-        game.Globals.Set("visualizer-settings", settings);
-
-        if (game.TryGetScreenScale(out var horizontal_scale, out var vertical_scale) &&
-            settings.AutomaticScaling) scale ??= (horizontal_scale + vertical_scale) / 2f;
-
-        game.Enqueue(instance =>
-        {
-            instance.SceneManager.LoadScene<Loader>("loader",
-                _ => new Loader(instance, audio_context)
-                {
-                    OnFinish = workflow => { OnLoadHandler(instance, workflow, sequence, greeting, scale); }
-                });
+            OnFinish = workflow => { OnLoadHandler(instance, workflow, sequence, greeting, scale); }
         });
+});
 
-        game.Enqueue(instance => instance.SceneManager.TransitionTo("loader"));
-        game.Run();
-    }
+game.Enqueue(instance => instance.SceneManager.TransitionTo("loader"));
+game.Run();
 
-    private static void OnLoadHandler(Game game, ThirtyDollarWorkflow workflow,
-        string? sequence, string? greeting, float? scale)
+return;
+        
+static void OnLoadHandler(Game game, ThirtyDollarWorkflow workflow,
+    string? sequence, string? greeting, float? scale)
+{
+    // preload all scenes in memory (inefficient i know, but leads to better UX)
+    game.Enqueue(instance =>
     {
-        // preload all scenes in memory (inefficient i know, but leads to better UX)
-        game.Enqueue(instance =>
-        {
-            instance.SceneManager.LoadScene<Home>("home", _ => new Home(instance, Visualizer.Version));
+        instance.SceneManager.LoadScene<Home>("home", _ => new Home(instance, Visualizer.Version));
 
-            instance.SceneManager.LoadScene<Visualizer>("visualizer", _ =>
-                new Visualizer(instance, SettingsHandler.Settings, workflow, [sequence])
-                {
-                    Greeting = greeting,
-                    Scale = scale ?? 1f
-                }
-            );
+        instance.SceneManager.LoadScene<Visualizer>("visualizer", _ =>
+            new Visualizer(instance, SettingsHandler.Settings, workflow, [sequence])
+            {
+                Greeting = greeting,
+                Scale = scale ?? 1f
+            }
+        );
 
-            instance.SceneManager.LoadScene<DrumMaster>("drum-master", _ => new DrumMaster(instance, workflow));
-            instance.SceneManager.LoadScene<Editor>("editor", _ => new Editor(instance));
-            instance.SceneManager.LoadScene<Settings>("settings", _ => new Settings(instance, SettingsHandler.Settings));
-        });
+        instance.SceneManager.LoadScene<DrumMaster>("drum-master", _ => new DrumMaster(instance, workflow));
+        instance.SceneManager.LoadScene<Editor>("editor", _ => new Editor(instance));
+        instance.SceneManager.LoadScene<Settings>("settings", _ => new Settings(instance, SettingsHandler.Settings));
+    });
 
-        game.Enqueue(instance => instance.SceneManager.TransitionTo("home"));
-    }
-
-    public class Options
-    {
-        [Option('i', "sequence", HelpText = "The sequence's location.")]
-        public string? Input { get; set; }
-
-        [Option("mode", HelpText = "Which mode the visualizer loads in.")]
-        public string? Mode { get; set; }
-
-        [Option("no-audio", HelpText = "Disable audio playback.")]
-        public bool NoAudio { get; set; }
-
-        [Option('w', "width", HelpText = "The width of the render window.")]
-        public int? Width { get; set; }
-
-        [Option('h', "height", HelpText = "The height of the render window.")]
-        public int? Height { get; set; }
-
-        [Option('c', "camera-follow-mode", HelpText = "Controls how the camera behaves. Values: \"tdw\", \"line\"")]
-        public string? CameraFollowMode { get; set; }
-
-        [Option('f', "fps-limit",
-            HelpText = "The fps cap of the renderer. Valid values are 0 - 500. Setting this to 0 removes the fps cap.")]
-        public int? Fps { get; set; }
-
-        [Option('s', "scale",
-            HelpText = "Changes the camera viewport zoom.")]
-        public float? Scale { get; set; }
-
-        [Option("audio-backend",
-            HelpText = "Changes the audio backend the application uses. Values: \"bass\", \"openal\"")]
-        public string? AudioBackend { get; set; }
-
-        [Option("greeting",
-            HelpText =
-                "Changes the default \'DON'T LECTURE ME WITH YOUR THIRTY DOLLAR VISUALIZER\' greeting. Supports emojis.")]
-        public string? Greeting { get; set; }
-
-        [Option("event-size", HelpText = "Changes how big the events are in pixels.")]
-        public int? EventSize { get; set; }
-
-        [Option("event-margin", HelpText = "Changes the distance between events in pixels.")]
-        public int? EventMargin { get; set; }
-
-        [Option("line-amount", HelpText = "Changes how many events are on a single line.")]
-        public int? LineAmount { get; set; }
-
-        [Option("settings-location",
-            HelpText = "Changes where the settings file is located. Default is: \'./Settings.30$\'")]
-        public string? SettingsLocation { get; set; }
-
-        [Option("transparent-framebuffer",
-            HelpText =
-                "Changes how the visualizer processes alpha rendering. If set the background of the window is rendered transparent and the OS decides how it'll use the transparency.")]
-        public bool? TransparentFramebuffer { get; set; }
-    }
+    game.Enqueue(instance => instance.SceneManager.TransitionTo("home"));
 }
