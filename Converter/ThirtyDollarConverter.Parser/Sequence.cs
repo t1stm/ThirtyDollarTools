a7b201a5f6bb4dc646208922a5f58497d755e430
Kristian Gergov <kristian@gergov.bg>
2026-07-12 17:22:38 +0300

    fix: prevent null multiplication errors in Sequence.cs defined events
    Signed-off-by: Kristian Gergov <kristian@gergov.bg>

--- Converter/ThirtyDollarConverter.Parser/Sequence.cs
+++ Converter/ThirtyDollarConverter.Parser/Sequence.cs
@@ -182,7 +182,7 @@ public partial class Sequence
                         {
                             SoundEvent = e.SoundEvent,
                             Value = !(e.SoundEvent ?? "").StartsWith('!') ? e.Value + parsed.Value : 0,
-                            Volume = e.Volume * ((parsed.Volume ?? 100) / 100),
+                            Volume = (e.Volume ?? 100) * ((parsed.Volume ?? 100) / 100),
                             ValueScale = e.ValueScale,
                         };
 
